70a0209fb9f77e734ffd71e44c4219090ef30509
Vlad <167doc@gmail.com>
2025-03-12 17:25:57 +0100

    Task 2 Completed

---
+++ task2.py
@@ -0,0 +1,14 @@
+# here's another feature file
+
+def odd_even(n):
+    if n %2 !=0:
+        return print("Weird")
+    else:
+        if n>=2 and n<=5:
+            return print("Not Weird")
+        
+        if n>=6 and n<=20:
+            return print("Weird")
+        
+        if n>20:
+            return print("Not Weird")
